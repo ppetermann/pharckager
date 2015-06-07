103ea77da06142d00af8ab734eec9fccb05c8cf0
Peter Petermann <peter.petermann@gameforge.com>
2015-06-07 16:38:27 +0200

    removing the phar-composer code, rebuilding own solution

--- Readme.md
+++ Readme.md
@@ -1,15 +1,9 @@
 # Pharckager
 
-A tool similar to phar-composer, so similar, that i even stole code!
+A tool to build phar packages.
 
-On a more serious note: this is heavy based on [phar-composer](https://github.com/clue/phar-composer) by clue, 
-and right now most of the code is code i stripped from phar-composer and have lightly modified to fall in with my own 
-command runner (opposed to the (probably better) symfony one that the original phar-composer is using).
+This is NOT stable software, use on your own risk.
 
-Now I could probably just have included phar-composer as a dependency at the current stage, but the plan is to make 
-some heavy modifications, and i will break compatibility (a lot). For now if you want a tool like this, head over to 
-clue's github, and grab phar-composer from there. While this might or might not work, it is most certainly not ready 
-to use yet. 
 
 ## Manual
 ... to come when it does more
@@ -25,7 +19,8 @@ MIT Style, see LICENSE.md
 
 ## CREDITS
 
-Well, there is a hand full of people whos code this project relies on, so its just fair to mention them here: 
- * Christian Lück Aka clue, the original author of phar-composer
+Well, there is a hand full of people which I should thank: 
+ * Christian Lück Aka clue, as I got inspired for this project by his phar-composer
  * The guys behind [box2-lib](https://github.com/box-project/box2-lib)
  * The [composer](https://getcomposer.org) guys
+ * Pádraic Brady, whos [phar-updater](https://github.com/padraic/phar-updater) I'm using.
--- src/Commands/BuildCommand.php
+++ src/Commands/BuildCommand.php
@@ -1,9 +1,10 @@
 <?php
 namespace Pharckager\Commands;
 
+use Herrera\Box\Box;
+use Herrera\Box\StubGenerator;
 use Knight23\Core\Banner\BannerInterface;
 use Knight23\Core\Output\WriterInterface;
-use Pharckager\Builder\Packager;
 
 class BuildCommand extends \Knight23\Core\Command\BaseCommand
 {
@@ -20,8 +21,9 @@ class BuildCommand extends \Knight23\Core\Command\BaseCommand
     public function __construct(WriterInterface $output, BannerInterface $banner)
     {
         $this->setName("build");
-        $this->setShort("build a specific package");
-        $this->addArgument('path/packagename', 'to build from (example: . for currenct directory, devedge/pharckager for pharckager..)');
+        $this->setShort("build a package from the current or given directory");
+        $this->addArgument('path', '.', 'to build from, defaults to .');
+        $this->addArgument('pharname', null, 'the name of the phar to build, defaults to directoryname.phar');
 
         $this->output = $output;
         $this->banner = $banner;
@@ -31,26 +33,32 @@ class BuildCommand extends \Knight23\Core\Command\BaseCommand
     {
         $this->output->writeln($this->banner->getBanner());
 
-        if (empty($arguments)) {
-            $this->output->write('<error>you have given no argument, what should i build?</error>'."\n");
-            die(1);
+        if (count($arguments) < 1) {
+            $path = ".";
+        } else {
+            $path = $arguments[0];
+        }
+        $path = realpath($path);
+
+        if (count($arguments) < 2) {
+            $pharname = basename($path).'.phar';
+        } else {
+            $pharname = $arguments[1];
+            if (substr($pharname, -5) !== '.phar') {
+                $pharname .= '.phar';
+            }
         }
 
-        $packager = new Packager();
+        $this->output->writeln("creating <info>$pharname</info> with contents from <info>$path</info>");
 
-        $output = $this->output;
-        $packager->setOutput(
-            function ($line) use ($output) {
-                $output->write($line);
-            }
+        $box = Box::create($pharname);
+        $box->buildFromDirectory($path);
+        $box->getPhar()->setStub(
+            StubGenerator::create()
+                ->index('bin/pharckager')
+                ->generate()
         );
 
-        $packager->coerceWritable();
-
-        $pharer = $packager->getPharer($arguments[0]);
-        if (count($arguments) > 1) {
-            $pharer->setTarget($arguments[1]);
-        }
-        $pharer->build();
+        $this->output->writeln("done");
     }
 }
